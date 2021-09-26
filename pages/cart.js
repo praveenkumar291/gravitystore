4575a7ed1bdf203cacc37cfd816bad625a83403e
praveen291 <mudadlapraveenkumar143@gmail.com>
2021-09-26 11:04:07 +0530

    subtotal

--- pages/cart.js
+++ pages/cart.js
@@ -1,8 +1,9 @@
-import { Grid, Typography } from '@material-ui/core';
+import { Button, Card, Grid, Link, List, ListItem, MenuItem, Select, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Typography, } from '@material-ui/core';
 import React, { useContext } from 'react'
 import Layout from '../components/Layout';
 import { Store } from '../utils/Store';
 import NextLink from 'next/link';
+import Image from 'next/image';
 
 
 
@@ -21,12 +22,89 @@ function cartScreen() {
           <Grid container spacing={1}>
             <Grid item md={9} xs={12}>
 
-              table of cart items
-            </Grid>
-            <Grid md={3} xs={12}>
+              <TableContainer >
+                <Table>
+                  <TableHead>
+                    <TableRow>
+                      <TableCell>
+                        image
+                      </TableCell>
+                      <TableCell>
+                        Name
+                      </TableCell>
+                      <TableCell align="right">
+                        Quantity
+                      </TableCell>
+                      <TableCell align="right">
+                        Price
+                      </TableCell>
+                      <TableCell align="right">
+                        Action
+                      </TableCell>
+                    </TableRow>
+                  </TableHead>
+                  <TableBody>
+                    {cartItems.map((item) => (
+                      <TableRow key={item._id}>
+                        <TableCell>
+                          <NextLink href={`/products/${item.slug}`} passHref>
+                            <Link>
+                              <Image src={item.image} alt={item.name} width={50} height={50}></Image>
+                            </Link>
+                          </NextLink>
 
-              cart actions
-            </Grid>
+                        </TableCell>
+                        <TableCell>
+                          <NextLink href={`/products/${item.slug}`} passHref>
+                            <Link>
+                              <Typography>{item.name}</Typography>
+                            </Link>
+                          </NextLink>
+
+                        </TableCell>
+                        <TableCell align="right">
+                        <Select value={item.quantity}>
+
+                          {[...Array(item.countInstocks).keys()].map((x) => (<MenuItem key={x + 1} value={x + 1}>{x + 1}</MenuItem>))}
+                         </Select>
+                        </TableCell>
+                        <TableCell align="right">
+                          ${item.price}
+                        </TableCell>
+                        <TableCell align="right">
+                          <Button variant="contained" color="secondary">x</Button>
+                        </TableCell>
+                      </TableRow>
+                    ))}
+                  </TableBody>
+                </Table>
+
+              </TableContainer>
+            </Grid >
+
+
+            <Grid  md={3} xs={12}>
+              <Card  >
+                <List >
+                  <ListItem >
+                    <Typography variant="h2">
+                      Subtotal ({cartItems.reduce((a, c) => a + c.quantity, 0)}{''}
+                      items)
+                      :$
+                      {cartItems.reduce((a, c) => a + c.quantity * c.price, 0)}
+
+                    </Typography>
+
+                  </ListItem>
+                  <ListItem>
+
+                    <Button variant="contained" color="primary" fullWidth>Checkout</Button>
+                  </ListItem>
+                </List>
+
+              </Card>
+
+            </Grid >
 
 
           </Grid>
